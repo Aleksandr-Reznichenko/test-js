4db6be6a6ec9a6f5a17e186735ad9c912638657b
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2023-10-20 21:38:59 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,30 +1,14 @@
 "use strict";
 
-// Задача 4. Доставка товару
+const start = 6;
+const end = 17;
+let number;
 
-function getShippingCost(country) {
-  let price;
-  switch (country) {
-    case "China":
-      price = 100;
-      return `Shipping to ${country} will cost ${price} credits`;
-    case "Chile":
-      price = 250;
-      return `Shipping to ${country} will cost ${price} credits`;
-    case "Australia":
-      price = 170;
-      return `Shipping to ${country} will cost ${price} credits`;
-    case "Jamaica":
-      price = 120;
-      return `Shipping to ${country} will cost ${price} credits`;
-
-    default:
-      return `Sorry, there is no delivery to your country`;
+for (let i = start; i <= end; i++) {
+  if (i % 5 === 0) {
+    number = i;
+    break;
   }
 }
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
-console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+console.log(number);
