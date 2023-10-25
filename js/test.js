01457dc76b90232d815df6c415288b58f564bbda
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2023-10-25 13:51:10 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,5 +1,20 @@
 'use strict';
 
-for (let i = 1; i <= 10; i += 2) {
-  console.log(i);
+// Задача 3. Фільтрація масиву чисел
+
+function filterArray(numbers, value) {
+  let numArray = [];
+
+  for (const iterElement of numbers) {
+    if (iterElement > value) {
+      numArray.push(iterElement);
+    }
+  }
+
+  return numArray;
 }
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
