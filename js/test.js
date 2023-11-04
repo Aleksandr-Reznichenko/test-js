1c23498c77bba9bda70a791a88d82457b7690801
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2023-11-04 11:00:17 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,20 +1,13 @@
 'use strict';
 
-// Задача 3. Фільтрація масиву чисел
+//
 
-function filterArray(numbers, value) {
-  let numArray = [];
-
-  for (const iterElement of numbers) {
-    if (iterElement > value) {
-      numArray.push(iterElement);
-    }
+function getMiddleSubstring(string) {
+  if (string.length) {
+  } else {
   }
-
-  return numArray;
 }
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+console.log(getMiddleSubstring('Hello')); // "l"
+console.log(getMiddleSubstring('JavaScript')); // "v"
+console.log(getMiddleSubstring('OpenAI')); // "pA"
