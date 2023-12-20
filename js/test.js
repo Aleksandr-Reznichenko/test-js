246f8d2c47b31768f71cc71aa1fc081838a7d5fb
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2023-12-20 16:04:58 +0200

    +

--- js/test.js
+++ js/test.js
@@ -1,15 +1,3 @@
 'use strict';
 
 // ======= >>>
-
-function passwordVerification(pass) {
-  if (pass.length < 8) {
-    return 'Пароль повинен містити не менше 8 символів.';
-  } else {
-  }
-}
-
-console.log(passwordVerification('FvLl!jbDSD54'));
-console.log(passwordVerification('FvLl!j'));
-console.log(passwordVerification('FvLlsjbDSD54'));
-console.log(passwordVerification('sjv1Ds32#5$'));
